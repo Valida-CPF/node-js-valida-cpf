4df43ad551027d6aa0fca1a3dbe11b86f2e75949
Gustavo Martins Paulo <gu.marti.paulo@gmail.com>
2020-10-15 12:53:03 -0300

    Added new status

--- src/checkdata.js
+++ src/checkdata.js
@@ -1,16 +1,23 @@
 exports.correctJSON = function (jsonTable) {
     for (var indice in jsonTable) {
-        var cpf = jsonTable[indice].CPF;
+        var cpf = jsonTable[indice].CPF;        
         
         cpf = cpf.trim();
         cpf = cpf.replace(/\./g, '');
         cpf = cpf.replace('-', '');
+
+        var aux = cpf.length;
+
         cpf = cpf.replace(/\D/g,'');
 
         jsonTable[indice].CPF_CHECK = cpf;
 
         if(cpf.length === 11){
-            jsonTable[indice].status = "Correto";
+            if(aux > cpf.length){
+                jsonTable[indice].status = "Corrigido";
+            }else{
+                jsonTable[indice].status = "Correto";
+            }            
         }else{
             jsonTable[indice].status = "Errado";
         }
